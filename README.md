c605083a330be3d513339106de5e1ae6d8d8c14d
Adrian Kreuziger <adrian@manifestdata.ai>
2023-05-20 15:14:08 -0700

    Base playbook

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ansible
 Ansible playbooks for common tasks
+
+## Running setup
+
+1. Run the ansible setup script,  This script makes sure that you have everything ready to run ansible.  If you have nothing installed, it takes about 5 to 10 minutes (mostly installing `homebrew`).  
+
+```bash
+sh scripts/setup-ansible.sh
+```
+
+2. (OSX) After the script runs, add the following to your ~/.zprofile:
+
+```bash
+# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(/opt/homebrew/bin/brew shellenv)"
+```
+
+## Running ansible
+
+
+### One-time Setup
+If the playbook has a `requirements.yml` file, install them
+
+```bash
+ansible-galaxy install -r base/requirements.yml
+```
+
+### Full
+Run this command from within the playbook directory
+```bash
+ansible-playbook -i inventory main.yml
+```
---
+++ scripts/setup-ansible.sh
@@ -0,0 +1,78 @@
+# Install prerequisites to running ansible
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "This script should not be run as root!"
+    exit 1
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo Detected Linux...
+        # Check for Ubuntu
+        if grep -q debian "/etc/os-release"; then
+            echo Detected debian distro...
+            # Install ansible via apt
+            echo "\n\nInstalling ansible from apt ..."
+            apt update
+            apt install -y software-properties-common
+            apt-add-repository --yes --update ppa:ansible/ansible
+            apt install -y ansible
+            apt install -y ansible-lint
+        # Check for alpine
+        else if grep -q alpine "/etc/os-release"; then
+            echo Detected alpine distro...
+            # Install ansible via apk
+            echo "\n\nInstalling ansible from apk ..."
+            apk add ansible
+            apk add ansible-lint
+        # Check for archlinux
+        else if grep -q archlinux "/etc/os-release"; then
+            echo Detected archlinux distro...
+            # Install ansible via pacman
+            echo "\n\nInstalling ansible from pacman ..."
+            pacman -Syu --noconfirm
+            pacman -S --noconfirm ansible
+            pacman -S --noconfirm ansible-lint
+        fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo Detected Mac OSX...
+        # Install homebrew
+        brew_path='/opt/homebrew/bin/brew'
+        if ! type brew >/dev/null; then
+            echo 'Installing Homebrew...'
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+            echo '\n\nAdding Homebrew to PATH...'
+            eval "$("$brew_path" shellenv)"
+
+            # If we can't find homebrew after installing, fail just to be safe
+            if ! type brew >/dev/null; then
+                echo '\n\nERROR: Homebrew installation failed. Please install it manually according to https://brew.sh/ and re-run this script.'
+                exit 1
+            fi
+        else
+            # If we see homebrew, update and proceed
+            echo "\n\nUpdating Homebrew ..."
+            brew update
+
+            eval "$("$brew_path" shellenv)"
+        fi
+
+        # Install ansible via Homebrew
+        echo "\n\nInstalling ansible from Homebrew ..."
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install ansible
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install ansible-lint
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        echo Detected POSIX compatibility layer and Linux environment emulation for Windows
+        echo This script is not supported on Cygwin
+elif [[ "$OSTYPE" == "msys" ]]; then
+        echo Detected Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        echo This script is not supported on MinGW
+elif [[ "$OSTYPE" == "win32" ]]; then
+        echo Detected Windows
+        echo This script is not supported on Windows
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        echo Detected FreeBSD
+        echo This script is not supported on FreeBSD
+else
+        echo Unknown OS $OSTYPE
+fi
